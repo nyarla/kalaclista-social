0cf2f33c0ddb117a92fd0a5c38e27718a5d2c334
nyarla <nyarla@kalaclista.com>
2023-11-26 20:17:56 +0900

    chore: restore litestream

--- src/litestream.nix
+++ src/litestream.nix
@@ -2,10 +2,8 @@
   dbs = [{
     path = "/data/sqlite3.db";
     replicas = [{
-      type = "s3";
-      bucket = "kalaclista-social-backup";
-      path = "sqlite3";
-      endpoint = "\${LITESTREAM_S3_ENDPOINT}";
+      url = "\${LITESTREAM_R2_URL}";
+      endpoint = "\${LITESTREAM_R2_ENDPOINT}";
     }];
   }];
 }
